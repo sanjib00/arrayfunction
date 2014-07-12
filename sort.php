9b575710ae5e95f747f39b2f236f664fef600add
Sanjib <sanjib@hotmail.com>
2014-07-12 21:50:49 +0600

    sort arrays in ascending order

--- sort.php
+++ sort.php
@@ -1,5 +1,17 @@
 <?php
-$age=array("Peter"=>"39","Ben"=>"37","Joe"=>"43");
-asort($age);
-print_r($age);
+//$age=array("Peter"=>"39","Ben"=>"37","Joe"=>"43");
+//asort($age);
+//print_r($age);
+
+
+$cars=array("Volvo","BMW","Toyota");
+sort($cars);
+
+$clength=count($cars);
+for($x=0;$x<$clength;$x++)
+   {
+   echo $cars[$x];
+   echo "<br>";
+   }
+
 ?>
